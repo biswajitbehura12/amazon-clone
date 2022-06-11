19d50aae6929b7213451d497e7802ebf55b086de
BISWAJIT BEHURA <102812316+biswajitbehura12@users.noreply.github.com>
2022-06-11 23:31:03 +0530

    ui part and sign in part completed

---
+++ src/components/firebaseAuth.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA2w0xlakSSDY9ShUYwVseyf1jHZjivbZI",
+  authDomain: "clone-89cb1.firebaseapp.com",
+  projectId: "clone-89cb1",
+  storageBucket: "clone-89cb1.appspot.com",
+  messagingSenderId: "466115076793",
+  appId: "1:466115076793:web:a026404c481f96eb9c3168",
+  measurementId: "G-0WTGJ3ZL1H"
+};
+  
